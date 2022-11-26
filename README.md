4ac706b5934bb2db4d378bf77f984a22f2762fbd
AnneScaunasu <scaunasu_anne.marie@yahoo.com>
2022-11-27 00:40:45 +0200

    Second push

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Wordle-proiect-ASC
+##Echipa
+-Scaunasu Anne-Marie (Grupa 144)
+-Grosu Andreea (Grupa 144)
+##Scurta descriere
+1)Programul "game.py" este jocul Wordle! care poate fi jucat de un player cu un numar nelimitat de incercari.
+2)Programul "wordle.py" ofera posibilitatea de a da algoritmului un cuvant sa il gaseasca in cat mai putine incercari (sau nu), apoi scrie intr-un fisier toate cuvintele si incercarile corespunzatoare pentru a-l gasi. Acest program isi termina rularea in aproximativ 20 de minute,iar la final scrie si media de incercari care este 4,46.
+In programul "wordle.py" este la inceput o sectiune '#algoritul intern'. In aceasta se fac calculele necesare:
+	-frecventa fiecrei litere pe fiecare pozitie
+	-probabilitatea fiecarui cuvant
+	-informatia oferita de fiecare cuvant
+	-entropia fiecarui cuvant
+	-cele mai bune 2 cuvinte de inceput
+In cardul sectiunii '#rezolvare' se fac urmatorii pasi sunt 2 parti ale programului separate printr-un if (daca se da cuvant de la tastaura sau nu) si algoritmul de ghicire a cuvintelor.
+Algoritul functioneaza astfel:
+	-se creeaza 7 vectori pentru:
+		1)literele gasite pe pozitiile bune (corect)
+		2)litera incercata pe prima pozitie, este in cuvant, dar nu pe pozitia incercata (in_cuv0)
+		3)litera incercata pe a doua pozitie, este in cuvant, dar nu pe pozitia incercata (in_cuv1)
+		4)litera incercata pe a treia pozitie, este in cuvant, dar nu pe pozitia incercata (in_cuv2)
+		5)litera incercata pe a patra pozitie, este in cuvant, dar nu pe pozitia incercata (in_cuv3)
+		6)litera incercata pe a cincea pozitie, este in cuvant, dar nu pe pozitia incercata (in_cuv4)
+		7)literele care nu se afla in cuvant (not_in_cuv)
+	<sub>Am incercat sa folosim o singura lista cu 5 liste componente, dar am avut probleme de verificare a acesteia in pasii ce urmeaza si am optat pentru o expunere mai usoara a acestora.</sub>
+	-daca este la primul pas ia cuvantul 'CAREI'(cuv1), acesta fiind ales pentru ca ne da cea mai multa informatie (cuvant care sa nu contina litere care sa se repete)
+	-daca este la al doilea pas ia cuvantul 'SOTUL'(cuv2), acesta fiind ales ca al doilea cuvant care ne da cea mai multa informatie fara sa repete litere din cuvantul anterior
+	-de la al treilea pas mai departe isi gaseste singur cuvantul din dictionarul de probabilitati a cuvintelor cu ajutorul conditiilor date prin cei 7 vectori
+	-fiecare solutie incercata este scrisa in fisier
+#Referinte
+https://www.inspiredpython.com/article/solving-wordle-puzzles-with-basic-python
+https://www.youtube.com/watch?v=v68zYyaEmEA
--- wordle.py
+++ wordle.py
@@ -2,9 +2,10 @@ f = open("cuvinte.txt", 'r')
 g = open("solutii.text", 'w')
 v = [list(x) for x in f.readlines() if x != "\n"]
 
-# programul intern
+# algoritmul intern
 
 import math
+import copy
 
 for x in v:
     del x[len(x) - 1]
@@ -13,7 +14,6 @@ fr2 = {}
 fr3 = {}
 fr4 = {}
 fr5 = {}
-frl = {}
 for x in range(26):
     fr1[chr(65 + x)] = 0
 for x in v:
@@ -44,23 +44,9 @@ for x in v:
     fr5[x[4]] += 1
 # print(f"A cincea pozitie:\n{fr5}")
 
-for x in range(26):
-    frl[chr(65 + x)] = 0
-for x in v:
-    for i in range(5):
-        frl[x[i]] += 1
-# print(f"Frecventa literelor in dictionarul dat este:\n{frl}")
-
-
-pr = {}
-for x in range(26):
-    pr[chr(65 + x)] = 0
-for x in frl:
-    pr[x] = frl[x] / (len(v) * 5)
-# print(f"Probabilitatea ca fiecare litera din alfabet sa apara in fisier este:\n{pr}")
-
-
 pr_cuv = {}
+info_cuv={}
+entropia={}
 for x in v:
     pr_cuv["".join(x)] = fr1[x[0]] / len(v)
     pr_cuv["".join(x)] += fr2[x[1]] / len(v)
@@ -68,79 +54,118 @@ for x in v:
     pr_cuv["".join(x)] += fr4[x[3]] / len(v)
     pr_cuv["".join(x)] += fr5[x[4]] / len(v)
     pr_cuv["".join(x)] /= 5
+    info_cuv["".join(x)] = (-1) * math.log2(pr_cuv["".join(x)])
+    entropia["".join(x)] = info_cuv["".join(x)] * pr_cuv["".join(x)]
+# print(f"Probabilitatea fiecarui cuvant este:\n{pr_cuv}")
+# print(f"Informatia data de ficare cuvant este:\n{info_cuv}")
+# print(f"Entropia fiecarui cuvant este:\n{pr_cuv}")
+
+for x in sorted(entropia, key=pr_cuv.get, reverse=True):
+    ok = 0
+    l = []
+    l.extend(x)
+    for i in l:
+        if l.count(i) == 1:
+            ok += 1
+    if ok == 5:
+        cuv1=l
+        break
+for x in sorted(entropia, key=pr_cuv.get, reverse=True):
+    ok = 0
+    for i in set(x):
+        if i not in set(cuv1):
+            ok += 1
+    if ok == 5:
+        l=[]
+        l.extend(x)
+        cuv2=l
+        break
 
 # rezolvare
 
-nr = 0
-x=list(input())
-nr_ok = 0
-corect = ["_", "_", "_", "_", "_"]
-in_cuv0 = []
-in_cuv1 = []
-in_cuv2 = []
-in_cuv3 = []
-in_cuv4 = []
-not_in_cuv = set()
-j=0
-while True:
-    if j == 0:
-        CUVANT = ['C', 'A', 'R', 'E', 'I']
-    elif j == 1:
-        CUVANT = ['S', 'O', 'T', 'U', 'L']
-    else:
-        for i in sorted(pr_cuv, key=pr_cuv.get, reverse=True):
-            ok1 = 0
-            ok2 = 0
-            ok3 = 0
-            for k in range(5):
-                if i[k] not in not_in_cuv:
-                    ok1 += 1
-            for k in range(len(in_cuv0)):
-                if (in_cuv0[k] in i) and i[0] != in_cuv0[k]:
-                    ok2 += 1
-            for k in range(len(in_cuv1)):
-                if (in_cuv1[k] in i) and i[1] != in_cuv1[k]:
-                    ok2 += 1
-            for k in range(len(in_cuv2)):
-                if (in_cuv2[k] in i) and i[2] != in_cuv2[k]:
-                    ok2 += 1
-            for k in range(len(in_cuv3)):
-                if (in_cuv3[k] in i) and i[3] != in_cuv3[k]:
-                    ok2 += 1
-            for k in range(len(in_cuv4)):
-                if (in_cuv4[k] in i) and i[4] != in_cuv4[k]:
-                    ok2 += 1
-            for k in range(5):
-                if corect[k] == '_' or corect[k] == i[k]:
-                    ok3 += 1
-            if ok1 == 5 and ok2 == nr_ok and ok3 == 5:
-                l = []
-                l.extend(i)
-                CUVANT = l
+
+print("Vrei sa lasi programul sa ghiceasca un cuvant?\nRaspunde cu 'DA' sau 'NU'")
+raspuns=input()
+ok=0
+nr=0
+while ok==0:
+    if raspuns =='DA':
+        print("Introduceti cuvantul de 5 litere(de tipar)")
+        ok=1
+        nr = 0
+        x=list(input())
+        nr_ok = 0
+        corect = ["_", "_", "_", "_", "_"]
+        in_cuv0 = []
+        in_cuv1 = []
+        in_cuv2 = []
+        in_cuv3 = []
+        in_cuv4 = []
+        not_in_cuv = set()
+        j=0
+        while True:
+            if j == 0:
+                CUVANT = copy.deepcopy(cuv1)
+            elif j == 1:
+                CUVANT = copy.deepcopy(cuv2)
+            else:
+                for i in sorted(pr_cuv, key=pr_cuv.get, reverse=True):
+                    ok1 = 0
+                    ok2 = 0
+                    ok3 = 0
+                    for k in range(5):
+                        if i[k] not in not_in_cuv:
+                            ok1 += 1
+                    for k in range(len(in_cuv0)):
+                        if (in_cuv0[k] in i) and i[0] != in_cuv0[k]:
+                            ok2 += 1
+                    for k in range(len(in_cuv1)):
+                        if (in_cuv1[k] in i) and i[1] != in_cuv1[k]:
+                            ok2 += 1
+                    for k in range(len(in_cuv2)):
+                        if (in_cuv2[k] in i) and i[2] != in_cuv2[k]:
+                            ok2 += 1
+                    for k in range(len(in_cuv3)):
+                        if (in_cuv3[k] in i) and i[3] != in_cuv3[k]:
+                            ok2 += 1
+                    for k in range(len(in_cuv4)):
+                        if (in_cuv4[k] in i) and i[4] != in_cuv4[k]:
+                            ok2 += 1
+                    for k in range(5):
+                        if corect[k] == '_' or corect[k] == i[k]:
+                            ok3 += 1
+                    if ok1 == 5 and ok2 == nr_ok and ok3 == 5:
+                        l = []
+                        l.extend(i)
+                        CUVANT = l
+                        break
+            print(str("".join(CUVANT)),end="")
+            if CUVANT == x:
+                print(f" ({j + 1} incercari)\n")
                 break
-    print(str("".join(CUVANT)),end="")
-    if CUVANT == x:
-        print(f" ({j + 1} incercari)\n")
-        break
-    print(" -> ",end="")
-    for i in range(5):
-        if CUVANT[i] == x[i]:
-            corect[i] = CUVANT[i]
-        elif CUVANT[i] in x:
-            nr_ok += 1
-            if i==0:
-                in_cuv0.append(CUVANT[i])
-            elif i==1:
-                in_cuv1.append(CUVANT[i])
-            elif i==2:
-                in_cuv2.append(CUVANT[i])
-            elif i==3:
-                in_cuv3.append(CUVANT[i])
-            elif i==4:
-                in_cuv4.append(CUVANT[i])
-        else:
-            not_in_cuv.add(CUVANT[i])
-    j+=1
+            print(" -> ",end="")
+            for i in range(5):
+                if CUVANT[i] == x[i]:
+                    corect[i] = CUVANT[i]
+                elif CUVANT[i] in x:
+                    nr_ok += 1
+                    if i==0:
+                        in_cuv0.append(CUVANT[i])
+                    elif i==1:
+                        in_cuv1.append(CUVANT[i])
+                    elif i==2:
+                        in_cuv2.append(CUVANT[i])
+                    elif i==3:
+                        in_cuv3.append(CUVANT[i])
+                    elif i==4:
+                        in_cuv4.append(CUVANT[i])
+                else:
+                    not_in_cuv.add(CUVANT[i])
+            j+=1
+    elif raspuns=='NU':
+        ok=1
+    else:
+        print("Raspunsul nu este corect! Scrieti un raspuns valid!")
 for x in v:
     nr_ok = 0
     g.write("\n")
@@ -156,9 +181,9 @@ for x in v:
     j=0
     while True:
         if j == 0:
-            CUVANT = ['C', 'A', 'R', 'E', 'I']
+            CUVANT = copy.deepcopy(cuv1)
         elif j == 1:
-            CUVANT = ['S', 'O', 'T', 'U', 'L']
+            CUVANT = copy.deepcopy(cuv2)
         else:
             for i in sorted(pr_cuv, key=pr_cuv.get, reverse=True):
                 ok1 = 0
@@ -215,6 +240,6 @@ for x in v:
                 not_in_cuv.add(CUVANT[i])
         j+=1
 nr /= len(v)
-g.write(f"Programul face aproximativ {nr:.2f} incercari pentru a gasi un cuvant")
+g.write(f"\nProgramul face aproximativ {nr:.2f} incercari pentru a gasi un cuvant")
 f.close()
 g.close()
